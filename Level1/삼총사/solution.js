0270a423c7a2ed76611505225aa71983024b98c0
yuj2n <dbwls4595@naver.com>
2026-04-19 05:09:24 +0900

    solve: 삼총사 문제풀이

---
+++ Level1/삼총사/solution.js
@@ -0,0 +1,14 @@
+function solution(number) {
+  var n = number.length;
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+}
